fe5fbc6142e925ace8a7d2c94df9996a4c973c00
F4250-Akif KA <karaozmehmetakif@gmail.com>
2022-10-02 13:10:56 +0300

    değişkenler-3

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //? Form elementlerinin yakalanarak değişkenlere atanması:
 
-//! Erkek ve Kadın Radio Butonları
+//! Mûris (Miras Bırakan): Erkek ve Kadın Radio Butonları
 const erkekRadioBtn = document.querySelector("#erkek"); //? Erkek
 const kadinRadioBtn = document.querySelector("#kadin"); //? Kadın
 
-//! Geride Kalanlar
+//! Mûrisin (Miras Bırakanın) Geride Bıraktıkları
 const kocasiCheckBtn = document.querySelector("#kocasi"); //? Kocası
 const hanimiInputNbr = document.querySelector("#hanimi"); //? Hanımı
 const dedesiCheckBtn = document.querySelector("#dedesi"); //? Dedesi
@@ -12,5 +12,11 @@ const ninesi_baCheckBtn = document.querySelector("#dedesi"); //? Ninesi (Babası
 const ninesi_aaCheckBtn = document.querySelector("#dedesi"); //? Ninesi (Annesinin Annesi)
 const babasiCheckBtn = document.querySelector("#babasi"); //? Babası
 const annesiCheckBtn = document.querySelector("#annesi"); //? Annesi
-const erkekKardesiA = document.querySelector("#erkek-kardesi-a"); //? Ana Bir Erkek Kardeşi
-const kizKardesiA = document.querySelector("#kiz-kardesi-a"); //? Ana Bir Erkek Kardeşi
+const erkekKardesiA_InputNbr = document.querySelector("#erkek-kardesi-a"); //? Ana Bir Erkek Kardeşi
+const kizKardesiA_InputNbr = document.querySelector("#kiz-kardesi-a"); //? Ana Bir Erkek Kardeşi
+const erkekKardesiAB_InputNbr = document.querySelector("#erkek-kardesi-ab"); //? Ana-Baba Bir Erkek Kardeşi
+const kizKardesiAB_InputNbr = document.querySelector("#erkek-kardesi-ab"); //? Ana-Baba Bir Kız Kardeşi
+const ogluInputNbr = document.querySelector("#oglu"); //? Oğlu
+const oglununOgluInputNbr = document.querySelector("#oglunun-oglu"); //? Oğlunun Oğlu
+const kiziInputNbr = document.querySelector("#kizi"); //? Kizi
+const oglununKiziInputNbr = document.querySelector("#oglunun-kizi"); //? Kizi
