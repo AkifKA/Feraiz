14551d5dc95d2f2dbcf48111837e2d8949eba3f6
F4250-Akif KA <karaozmehmetakif@gmail.com>
2022-10-02 13:01:54 +0300

    değişkenler-1

---
+++ app.js
@@ -0,0 +1,16 @@
+//? Form elementlerinin yakalanarak değişkenlere atanması:
+
+//! Erkek ve Kadın Radio Butonları
+const erkekRadioBtn = document.querySelector("#erkek"); //? Erkek
+const kadinRadioBtn = document.querySelector("#kadin"); //? Kadın
+
+//! Geride Kalanlar
+const kocasiCheckBtn = document.querySelector("#kocasi"); //? Kocası
+const hanimiInputNbr = document.querySelector("#hanimi"); //? Hanımı
+const dedesiCheckBtn = document.querySelector("#dedesi"); //? Dedesi
+const ninesi_baCheckBtn = document.querySelector("#dedesi"); //? Ninesi (Babasının Annesi)
+const ninesi_aaCheckBtn = document.querySelector("#dedesi"); //? Ninesi (Annesinin Annesi)
+const babasiCheckBtn = document.querySelector("#babasi"); //? Babası
+const annesiCheckBtn = document.querySelector("#annesi"); //? Annesi
+const erkekKardesiA = document.querySelector("#erkek-kardesi-a"); //? Ana Bir Erkek Kardeşi
+const kizKardesiA = document.querySelector("#kiz-kardesi-a"); //? Ana Bir Erkek Kardeşi
