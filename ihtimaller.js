b9952c770f47c771921d75b9dcd4269ff6fa2f7b
F4250-Akif KA <karaozmehmetakif@gmail.com>
2022-10-02 12:20:28 +0300

    2.10.22

---
+++ ihtimaller.js
@@ -0,0 +1,108 @@
+//? B: KALAN, S: SAKIT
+
+//! KADIN SEÇİLDİĞİNDE
+
+//? Durum 1: Sadece Koca
+//? İhtimal 1: Sadece Koca
+
+//? Durum 2: Koca ve Çocuklu (3)
+//? İhtimal 1: Koca + Çocuk
+//? İhtimal 2: Koca + Ogul
+//? İhtimal 3: Koca + Kiz
+
+//? Durum 3: Sadece çocuklar (3)
+//? İhtimal 1: Sadece Erkekler
+//? İhtimal 2: Sadece Kızlar
+//? İhtimal 3: Erkekler ve Kizlar
+
+//? Durum 4: Sadece Baba
+//? İhtimal 1: Sadece Baba
+
+//? Durum 5: Baba ve Koca ve Çocuklar ve Anne ve Kardeşlerden Herhangi Biri
+//? İhtimal 1: Baba (B) + Koca (1/2)
+//? İhtimal 2: Baba + Koca + Oğul
+//? İhtimal 3: Baba + Koca + Kiz
+//? İhtimal 4: Baba + Koca + Oğul + Kiz
+//? İhtimal 5: Baba + Koca + O.Oğul + Kiz (S)
+//? İhtimal 6: Baba + Koca + O.Kizi (1/2) + Kiz (1/6)
+//? İhtimal 7: Baba + Koca + 2 O.Kizi (1/6) + 1 Kiz (1/2)
+//? İhtimal 8: Baba + Koca + O.Kizi (S) + 2 Kiz (2/3)
+//? İhtimal 9: Baba + Koca + Anne
+//? İhtimal 10: Baba + Koca + Kardeşlerden Herhangi Biri (S)
+//? İhtimal 11: Baba (2/3)+ Anne(1/3)
+//? İhtimal 12: Baba + Anne + Oğul
+//? İhtimal 13: Baba + Anne + Kız
+//? İhtimal 14: Baba + Anne + Oğul + Kız
+//? İhtimal 15: Baba + Anne + O.Kızı + Kız
+//? İhtimal 16: Baba + Anne + O.Oğlu + Kız (S)
+//? İhtimal 17: Baba + Oğul
+//? İhtimal 18: Baba + Kız
+//? İhtimal 19: Baba + Oğul + Kız
+//? İhtimal 20: Baba + O.Oğlu + O. Kızı
+//? İhtimal 21: Baba + Kız + O. Kızı
+//? İhtimal 22: Baba + 2 O.Kizi + 1 Kiz
+//? İhtimal 23: Baba + O.Kizi + Kiz
+
+//? Durum 6: Anne ve Koca ve Çocuklar
+
+//? İhtimal 1 : Anne
+//? İhtimal 2 : Anne + Koca
+//? İhtimal 3: Anne + Koca + Oğul
+//? İhtimal 4: Anne + Koca + Kız
+//? İhtimal 5: Anne + Koca + Oğul + Kız
+//? İhtimal 6: Anne + Koca + O. Oğlu
+//? İhtimal 7: Anne + Koca + O. Kızı
+//? İhtimal 8: Anne + Koca + O. Kızı + Kız
+//? İhtimal 9: Anne + Koca + 2 O. Kızı + Kız
+//? İhtimal 10: Anne + Koca + O. Kızı + 2 Kız
+//? İhtimal 11: Anne + Oğul
+//? İhtimal 12: Anne + Kız
+//? İhtimal 13: Anne + Oğul + Kız
+//? İhtimal 14: Anne + O.Oğlu + O. Kızı
+//? İhtimal 15: Anne + Kız + O. Kızı
+//? İhtimal 16: Anne + 2 O.Kizi + 1 Kiz
+//? İhtimal 17: Anne + O.Kizi + Kiz
+
+//! Durumlar
+// koca: 1
+// koca + baba: 5
+// koca + anne : 6
+// koca + çocuk : 2
+// koca + baba + anne : 5
+// koca + baba + çocuk : 5
+// koca + anne + çocuk :6
+// baba : 4
+// baba + anne : 5
+// baba + çocuk : 5
+// baba + anne + çocuk: 5
+// anne: 6
+// anne + çocuk : 6
+// çocuk: 3
+// koca + baba + anne + çocuk: 5
+
+//? İhtimal 10: Anne + Koca + Oğul + Kız + Kız Kardeş (S)
+//? İhtimal 11: Anne + Koca + Kız + A.B. Erkek Kardeş (B)
+//? İhtimal 11: Anne + Koca + Kız (6/12) + A.B Kız Kardeş (1/12)
+//? İhtimal 11: Anne + Koca + Kız + B. Erkek Kardeş
+//? İhtimal 11: Anne + Koca + Kız + B. Kız Kardeş
+
+//! koca + o.oglu + kiz
+//! koca + o.kizi + kiz
+//! koca + ogul + o.kizi + kiz >>
+//! koca (1/4) + o.kizi (1/2) + kiz (1/6) >> Kalan oranlar nispetinde dağıtılır
+
+//? Durum 4:
+
+//! ERKEK SEÇİLDİĞİNDE
+//? Durum 1: Sadece Kari
+//? İhtimal 1: Sadece Kari
+
+//? Durum 2: Kari ve Çocuklu (3)
+//? İhtimal 1: Kari + Çocuk
+//? İhtimal 2: Kari + Ogul
+//? İhtimal 3: Kari + Kiz
+
+//? Durum 3: Sadece çocuklar (3)
+//? İhtimal 1: Sadece Erkekler
+//? İhtimal 2: Sadece Kızlar
+//? İhtimal 3: Erkekler ve Kizlar
